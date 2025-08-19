350cc8b887e2d4f9164b4bf1919646dc22a6b5eb
Rob Skilling <rob@dxw.com>
2025-08-19 09:40:57 +0100

    Update extensions list

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,12 +1,20 @@
-#!/bin/sh
 code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension csstools.postcss
+code --install-extension davidanson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension docker.docker
+code --install-extension editorconfig.editorconfig
 code --install-extension felixfbecker.php-intellisense
+code --install-extension getpsalm.psalm-vscode-plugin
+code --install-extension github.vscode-github-actions
 code --install-extension golang.go
-code --install-extension IronGeek.vscode-env
+code --install-extension irongeek.vscode-env
+code --install-extension ms-azuretools.vscode-containers
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ronnidc.nunjucks
 code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension shopify.ruby-lsp
 code --install-extension whatwedo.twig
 code --install-extension xdebug.php-debug
 code --install-extension xdebug.php-pack
